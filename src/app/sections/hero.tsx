0372ab721b261335b41042fb3e6e9406c568e279
Agustin Galante <agustin.galante.19@outlook.es>
2024-02-28 03:18:56 -0300

    Basic hero section

---
+++ src/app/sections/hero.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/styles/hero.module.css"
+
+function Hero() {
+  return (
+    <div>
+      <h1 className={styles.title}>Personal Expenses</h1>
+    </div>
+  )
+}
+
+export default Hero
